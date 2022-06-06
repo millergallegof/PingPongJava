c4ead5485b5532780ef469d9eed95e595b3e98a2
millergallegof <miller.gallegof@gmail.com>
2022-06-06 18:52:54 -0500

    Cambiando de cordenadas las barras

--- main.js
+++ main.js
@@ -27,16 +27,19 @@
         this.board = board
         this.board.bars.push(this)
         this.kind = "rectangle"
-
+        this.speed = 10;
     }
 
     self.Bar.prototype = {
 
         down: function () {
-
+            this.y += this.speed;
         },
         up: function () {
-
+            this.y -= this.speed;
+        },
+        toString: function () {
+            return "x: " + this.x + " y: " + this.y;
         }
     }
 
@@ -74,15 +77,26 @@
 
 })();
 
+var board = new Board(800, 400)
+var bar = new Bar(20, 100, 40, 100, board)
+var bar = new Bar(735, 100, 40, 100, board)
+var canvas = document.getElementById('canvas');
+var board_view = new BoardView(canvas, board);
+
+document.addEventListener('keydown', function (e) {
+    if (e.keyCode == 38) {
+        bar.up();
+    } else if (e.keyCode == 40) {
+        bar.down();
+    }
+    console.log("" + bar);
+})
+
 window.addEventListener('load', main())
 
 
 function main() {
-    var board = new Board(800, 400)
-    var bar = new Bar(20, 100, 40, 100, board)
-    var bar = new Bar(735, 100, 40, 100, board)
-    var canvas = document.getElementById('canvas');
-    var board_view = new BoardView(canvas, board);
+
     console.log(board);
     board_view.draw();
 }
