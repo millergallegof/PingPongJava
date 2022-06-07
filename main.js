4b835326c90754d97ae1898049eabcd6cb169b72
millergallegof <miller.gallegof@gmail.com>
2022-06-06 23:28:14 -0500

    Creacion colision en las barras de juego

--- main.js
+++ main.js
@@ -29,7 +29,11 @@
         this.speed_y = 0;
         this.speed_x = 3;
         this.board = board;
-        this.direction = 1
+        this.direction = -1
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
+
 
         board.ball = this;
         this.kind = "circle"
@@ -39,6 +43,32 @@
         move: function () {
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+        },
+        get width() {
+            return this.radius * 2;
+        }
+        ,
+        get height() {
+            return this.radius * 2;
+        }
+        ,
+        collision: function (bar) {
+            var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+            var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_y * this.max_bounce_angle
+
+            console.log(this.bounce_angle);
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if (this.x > (this.board.width / 2)) {
+                this.direction = -1;
+            } else { this.direction = 1; }
+
+
         }
     }
 
@@ -92,14 +122,44 @@
                 draw(this.ctx, element)
             }
         },
+        check_collisions: function () {
+            for (let index = this.board.bars.length - 1; index >= 0; index--) {
+                var bar = this.board.bars[index]
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
         play: function () {
             if (this.board.playing) {
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
             }
         }
 
+    }
+
+    function hit(a, b) {
+        var hit = false;
+
+        if (b.x + b.width >= a.x && b.x < a.x + a.width) {
+            if (b.y + b.height >= a.y && b.y < a.y + a.height) {
+                hit = true
+            }
+        }
+        if (b.x <= a.x && b.x + b.width >= a.x + a.width) {
+            if (b.y <= a.y && b.y + b.height >= a.y + a.height) {
+                hit = true
+            }
+        }
+        if (a.x <= b.x && a.x + a.width >= b.x + b.width) {
+            if (a.y <= b.y && a.y + a.height >= b.y + b.height) {
+                hit = true
+            }
+        }
+        return hit
     }
 
     function draw(ctx, element) {
